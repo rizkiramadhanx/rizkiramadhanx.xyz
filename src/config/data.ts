723cd9f3892c5aaf53dd404f91126a87053e534a
Rizki Ramadhan <rizkijitu77@gmail.com>
2023-04-30 18:57:27 +0700

    chore : add project

--- src/config/data.ts
+++ src/config/data.ts
@@ -2,6 +2,8 @@ import Suxz from '@/assets/project/Suxz-landing-page.png';
 import QuranApp from '@/assets/project/Quran.png';
 import Dekoor from '@/assets/project/Dekoor.png';
 import Pahein from '@/assets/project/Pahein.png';
+import Infralib from '@/assets/project/Infralib.png';
+import TodoApp from '@/assets/project/TodoApp.png';
 
 const data = {
   project: {
@@ -9,7 +11,7 @@ const data = {
       {
         description: `This online course platform features a captivating hero section with an illustration on the right and a prominent call-to-action button. Below the hero section, you'll find a comprehensive list of available classes. The "About Us" section provides a brief overview of the platform's mission and values. The platform's partnerships with reputable companies are highlighted in the "Companies Partners" section. Testimonials from satisfied alumni add credibility to the platform in the "Testimonials" section. Finally, a clear and compelling call-to-action is displayed at the bottom of the page, encouraging visitors to take the next step in their education.`,
         image: Suxz,
-        technology: ['React', 'Tailwind'],
+        technology: ['React', 'TailwindCSS'],
         title: 'Suxz Landing Page',
         url: {
           deploy: 'https://suxz-codedesign-zeta.vercel.app/',
@@ -28,7 +30,7 @@ const data = {
       {
         description: `The Quran App is a pocket Quran application that is simple and easy for Muslims to read the Quran anywhere and anytime.`,
         image: QuranApp,
-        technology: ['React', 'Tailwind', 'API'],
+        technology: ['React', 'TailwindCSS', 'Public API'],
         title: 'The Quran App',
         url: {
           deploy: 'https://quran-rizkiramadhanx.vercel.app/',
@@ -38,10 +40,30 @@ const data = {
       {
         description: `Dekoor is a web application that provides quality furniture`,
         image: Dekoor,
-        technology: ['React', 'Tailwind'],
+        technology: ['React', 'TailwindCSS'],
         title: 'Dekoor Landing Page',
         url: {
           deploy: 'https://dekoor-theta.vercel.app/',
+        },
+      },
+      {
+        description:
+          'Infralib (Indonesia Infrastructure Library) is an application owned by PT SMI for sharing knowledge and experiences related to infrastructure development. Infralib has two platforms, landing page and backoffice.',
+        image: Infralib,
+        title: 'Infralib - PT Sarana Multi Infrastruktur',
+        technology: ['NextJS', 'React-Query', 'TailwindCSS'],
+        url: {
+          deploy: 'https://www.dot.co.id/works/knowledge-management-system',
+        },
+      },
+      {
+        description:
+          'A to-do list web app that created when i joined Devcode Frontend Challenge by Gethired.id few days ago. The app contains some basic features like adding, updating, and removing todos. Todos are grouped inside an activity.',
+        image: TodoApp,
+        title: 'Todo App Chalenge',
+        technology: ['API', 'TailwindCSS'],
+        url: {
+          deploy: 'https://todo-get-hired.vercel.app/',
         },
       },
     ],

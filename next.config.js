03a652c5768032d29884f808a1b98e6f3339aa3c
Rizki Ramadhan <rizkijitu77@gmail.com>
2023-07-22 17:05:08 +0700

    feat : add test mdx, add navbar

--- next.config.js
+++ next.config.js
@@ -2,8 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['picsum.photos'],
+    domains: ["picsum.photos"],
   },
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 };
 
-module.exports = nextConfig;
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+});
+
+module.exports = withMDX(nextConfig);
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,23 +1,23 @@
-import clsxm from '@/libs/clsxm';
+import clsxm from "@/libs/clsxm";
 
-import { useState, useEffect } from 'react';
-import styles from '@/styles/styles';
-import ThemeButton from '@/components/button/ThemeButton';
-import { useRouter } from 'next/router';
-import Link from 'next/link';
+import { useState, useEffect } from "react";
+import styles from "@/styles/styles";
+import ThemeButton from "@/components/button/ThemeButton";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 const navbarDatas = [
   {
-    pathname: '/',
-    name: 'Home',
+    pathname: "/",
+    name: "Home",
   },
   {
-    pathname: '/project',
-    name: 'Project',
+    pathname: "/project",
+    name: "Project",
   },
   {
-    pathname: '/now',
-    name: 'Now',
+    pathname: "/now",
+    name: "Now",
   },
 ];
 
@@ -33,22 +33,22 @@ const Header = () => {
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   });
   return (
     <nav
       className={clsxm(
-        'backdrop-blur-md bg-opacity-20',
-        'sticky z-10 top-0',
+        "backdrop-blur-md bg-opacity-20",
+        "sticky z-10 top-0",
         onTop
-          ? ''
-          : ' dark:shadow-white dark:bg-mainDark shadow-black bg-main border-b border-gray-300'
+          ? ""
+          : " dark:shadow-white dark:bg-mainDark shadow-black bg-main border-b border-gray-300"
       )}
     >
-      <div className={clsxm('h-16', styles.container)}>
+      <div className={clsxm("h-16", styles.container)}>
         <div className="flex items-center justify-between h-full">
           <div className="flex items-center">
             <div className="hidden sm:inline font-bold text-lg">
@@ -60,21 +60,41 @@ const Header = () => {
                   href={data.pathname}
                   key={key}
                   className={clsxm(
-                    pathname === data.pathname ? 'px-1 ' : '',
-                    'cursor-pointer hover:scale-105 text-underline'
+                    pathname === data.pathname ? "px-1 " : "",
+                    "cursor-pointer hover:scale-105 text-underline"
                   )}
                 >
                   <span
                     className={clsxm(
                       pathname === data.pathname
-                        ? 'text-gradient-sm font-extrabold'
-                        : ''
+                        ? "text-gradient-sm font-extrabold"
+                        : ""
                     )}
                   >
                     {data.name}
                   </span>
                 </Link>
               ))}
+              <Link
+                href="/blog"
+                className={clsxm(
+                  pathname === "blog" ? "px-1 " : "",
+                  "cursor-pointer hover:scale-105 hidden lg:block"
+                )}
+              >
+                <span
+                  className={clsxm(
+                    pathname === "blog"
+                      ? "text-gradient-sm font-extrabold"
+                      : "" + "text-underline"
+                  )}
+                >
+                  Code{" "}
+                  <sup className="text-xs font-bold text-orange-500 bg-purple-200 px-1 py-1 rounded-md">
+                    for developer
+                  </sup>
+                </span>
+              </Link>
             </div>
           </div>
           <ThemeButton />

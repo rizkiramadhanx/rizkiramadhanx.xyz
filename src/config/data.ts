010f1d717f153987c526d3dfb9a2072650ea4596
Rizki Ramadhan <rizkijitu77@gmail.com>
2023-11-28 13:22:53 +0700

    feat : add komcards

--- src/config/data.ts
+++ src/config/data.ts
@@ -6,10 +6,22 @@ import Infralib from "@/assets/project/Infralib.png";
 import TodoApp from "@/assets/project/TodoApp.png";
 import Komship from "@/assets/project/Komship.png";
 import SIPUS from "@/assets/project/SIPUS.jpg";
+import Komcards from "@/assets/project/Komcards.png"
 
 const data = {
   project: {
     card: [
+      {
+        image: Komcards,
+        title: 'Komcards',
+        description:
+          "Komcards is a platform from Komerce that can simplify the payment process for all your business needs online. We are committed to continuing to support the domestic economy by making it easier to do business for MSMEs and online business people.",
+        technology: ["Bootstrap", "NuxtJS"],
+        url: {
+          deploy: "https://komcards.id/",
+        }
+      },
+
       {
         description: `This online course platform features a captivating hero section with an illustration on the right and a prominent call-to-action button. Below the hero section, you'll find a comprehensive list of available classes. The "About Us" section provides a brief overview of the platform's mission and values. The platform's partnerships with reputable companies are highlighted in the "Companies Partners" section. Testimonials from satisfied alumni add credibility to the platform in the "Testimonials" section. Finally, a clear and compelling call-to-action is displayed at the bottom of the page, encouraging visitors to take the next step in their education.`,
         image: Suxz,
